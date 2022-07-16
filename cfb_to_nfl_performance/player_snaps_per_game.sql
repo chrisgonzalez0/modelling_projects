5731a8443c4da4f38ebc171ea5bd4fdd2a49a37d
Christopher Gonzalez <chrisgonza@gmail.com>
2022-07-16 14:53:56 -0500

    Update player_snaps_per_game.sql
    add final boxscore stats to dataset, remove group by team and just accumulate stats by player in first 3 years

--- cfb_to_nfl_performance/player_snaps_per_game.sql
+++ cfb_to_nfl_performance/player_snaps_per_game.sql
@@ -36,8 +36,7 @@ snapcount as (
 	sg."Def._Num" as def_num,
 	sg."Def._Pct" as def_pct,
 	sg."ST_Num" as st_num,
-	sg."ST_Pct" as st_pct
-	
+	sg."ST_Pct" as st_pct	
 	from snapcount_games sg 
 	left join nfl_team_rosters ntr1 on sg.player_id=ntr1.player_id and sg.away_team=ntr1.team_id 
 	left join nfl_team_rosters ntr2 on sg.player_id=ntr2.player_id and sg.home_team=ntr2.team_id 
@@ -48,8 +47,50 @@ base_stats as (
 	select 
 		pk.college_id,
 		sc.*,
-		ntr.yrs,ntr.age,ntr.ht,ntr.wt,ntr.pos
-		--,boxscores.*
+		ntr.yrs,ntr.age,ntr.ht,ntr.wt,ntr.pos,
+		nts."year",
+		boxscores."Passing_Cmp",
+		boxscores."Passing_Att",
+		boxscores."Passing_Yds",
+		boxscores."Passing_TD",
+		boxscores."Passing_Int",
+		boxscores."Passing_Sk",
+		boxscores."Passing_Sk_Yds",
+		boxscores."Rushing_Att",
+		boxscores."Rushing_Yds",
+		boxscores."Rushing_TD",
+		boxscores."Receiving_Tgt",
+		boxscores."Receiving_Rec",
+		boxscores."Receiving_Yds",
+		boxscores."Receiving_TD",		
+		boxscores."Fumbles_Fmb",
+		boxscores."Fumbles_FL",		
+		boxscores."Def Interceptions_Int",
+		boxscores."Def Interceptions_Yds",		
+		boxscores."Def Interceptions_TD",
+		boxscores."Def Interceptions_PD",
+		boxscores."Sk",
+		boxscores."Tackles_Comb",		
+		boxscores."Tackles_Solo",
+		boxscores."Tackles_Ast",		
+		boxscores."Tackles_TFL",		
+		boxscores."Tackles_QBHits",
+		boxscores."Fumbles_FR",
+		boxscores."Fumbles_Yds",		
+		boxscores."Fumbles_TD",
+		boxscores."Fumbles_FF",
+		boxscores."Kick Returns_Rt",		
+		boxscores."Kick Returns_Yds",
+		boxscores."Kick Returns_TD",
+		boxscores."Punt Returns_Ret",		
+		boxscores."Punt Returns_Yds",
+		boxscores."Punt Returns_TD",		
+		boxscores."Scoring_XPM",		
+		boxscores."Scoring_XPA",
+		boxscores."Scoring_FGM",
+		boxscores."Scoring_FGA",		
+		boxscores."Punting_Pnt",
+		boxscores."Punting_Yds"		
 	from snapcount sc
 	left join nfl_team_schedules nts on nts.team_id=sc.team_id and nts.boxscore_id=sc.boxscore_id
 	left join nfl_team_rosters ntr on ntr.player_id=sc.player_id and ntr.team_id=sc.team_id and ntr."year"=nts."year" 
@@ -105,6 +146,7 @@ base_stats as (
 			coalesce(cast(nbk."Scoring_FGA" as float),0) as "Scoring_FGA",
 			coalesce(cast(nbk."Punting_Pnt" as float),0) as "Punting_Pnt",
 			coalesce(cast(nbk."Punting_Yds" as float),0) as "Punting_Yds"
+			
 		from nfl_boxscore_offense nbo 
 		full outer join nfl_boxscore_defense nbd on nbd.player_id=nbo.player_id and nbd.boxscore_id=nbo.boxscore_id 
 		full outer join nfl_boxscore_kick_returns nbkr on nbkr.player_id=coalesce(nbo.player_id,nbd.player_id) and nbkr.boxscore_id=coalesce(nbo.boxscore_id,nbd.boxscore_id) 
@@ -113,7 +155,12 @@ base_stats as (
 	left join player_keys pk on pk.player_id=sc.player_id
 	where 
 		ntr.yrs in ('Rook','1','2')
+		
+		and sc.player_id in (select distinct ntr.player_id 
+		                     from nfl_team_rosters ntr 
+		                     where ntr.yrs='Rook' and ntr."year" >= 2012)		
 ) 
+
 
 /* This has the total number of 
    snaps played in rookie season */
@@ -122,17 +169,68 @@ select
 	bs.player_id,
 	bs."Player",
 	bs."Pos",
-	bs.team_id,
-	sum(bs.off_num+bs.def_num+bs.st_num)
+	count(distinct bs.yrs) as years_played,
+	sum(bs.off_num+bs.def_num+bs.st_num) as snapcounts,
+	min(min_year.first_year) as first_year,
+	sum(bs."Passing_Cmp") "Passing_Cmp",
+	sum(bs."Passing_Att") "Passing_Att",
+	sum(bs."Passing_Yds") "Passing_Yds",
+	sum(bs."Passing_TD") "Passing_TD",
+	sum(bs."Passing_Int") "Passing_Int",
+	sum(bs."Passing_Sk") "Passing_Sk",
+	sum(bs."Passing_Sk_Yds") "Passing_Sk_Yds",
+	sum(bs."Rushing_Att") "Rushing_Att",
+	sum(bs."Rushing_Yds") "Rushing_Yds",
+	sum(bs."Rushing_TD") "Rushing_TD",
+	sum(bs."Receiving_Tgt") "Receiving_Tgt",
+	sum(bs."Receiving_Rec") "Receiving_Rec",
+	sum(bs."Receiving_Yds") "Receiving_Yds",
+	sum(bs."Receiving_TD") "Receiving_TD",		
+	sum(bs."Fumbles_Fmb") "Fumbles_Fmb",
+	sum(bs."Fumbles_FL") "Fumbles_FL",		
+	sum(bs."Def Interceptions_Int") "Def Interceptions_Int",
+	sum(bs."Def Interceptions_Yds") "Def Interceptions_Yds",		
+	sum(bs."Def Interceptions_TD") "Def Interceptions_TD",
+	sum(bs."Def Interceptions_PD") "Def Interceptions_PD",
+	sum(bs."Sk") "Sk",
+	sum(bs."Tackles_Comb") "Tackles_Comb",		
+	sum(bs."Tackles_Solo") "Tackles_Solo",
+	sum(bs."Tackles_Ast") "Tackles_Ast",		
+	sum(bs."Tackles_TFL") "Tackles_TFL",		
+	sum(bs."Tackles_QBHits") "Tackles_QBHits",
+	sum(bs."Fumbles_FR") "Fumbles_FR",
+	sum(bs."Fumbles_Yds") "Fumbles_Yds",		
+	sum(bs."Fumbles_TD") "Fumbles_TD",
+	sum(bs."Fumbles_FF") "Fumbles_FF",
+	sum(bs."Kick Returns_Rt") "Kick Returns_Rt",		
+	sum(bs."Kick Returns_Yds") "Kick Returns_Yds",
+	sum(bs."Kick Returns_TD") "Kick Returns_TD",
+	sum(bs."Punt Returns_Ret") "Punt Returns_Ret",		
+	sum(bs."Punt Returns_Yds") "Punt Returns_Yds",
+	sum(bs."Punt Returns_TD") "Punt Returns_TD",		
+	sum(bs."Scoring_XPM") "Scoring_XPM",		
+	sum(bs."Scoring_XPA") "Scoring_XPA",
+	sum(bs."Scoring_FGM") "Scoring_FGM",
+	sum(bs."Scoring_FGA") "Scoring_FGA",		
+	sum(bs."Punting_Pnt") "Punting_Pnt",
+	sum(bs."Punting_Yds") "Punting_Yds"		
+	
 from base_stats bs
-where bs."Pos"='RB'
+left join (
+	select 
+	bs.player_id,
+	min(bs."year") as first_year
+	from base_stats bs 
+	group by bs.player_id
+) min_year on min_year.player_id=bs.player_id
+where bs."Pos"='WR'
 group by 
 	bs.college_id,
 	bs.player_id,
 	bs."Player",
-	bs."Pos",
-	bs.team_id
+	bs."Pos"
 order by 
+	min(min_year.first_year) desc,
 	sum(bs.off_num+bs.def_num+bs.st_num) desc
 	
 	
