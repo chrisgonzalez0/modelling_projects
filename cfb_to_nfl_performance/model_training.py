d40819b2dce6207a5a2a8fe0dffd7500149f0793
Christopher Gonzalez <chrisgonza@gmail.com>
2022-07-21 14:38:28 -0500

    Update model_training.py
    continue model development, save means and standard devs for data cleaning

--- cfb_to_nfl_performance/model_training.py
+++ cfb_to_nfl_performance/model_training.py
@@ -21,6 +21,35 @@ samples=pd.read_sql_query(sql=sql_query, con=engine)
 #samples.groupby(['first_year']).count()
 
 
+sql_query='select * from cfb_player_query_table p where p."year" >= 2008'
+samp=pd.read_sql_query(sql=sql_query, con=engine)
+
+
+x_numerical_cols=['Pts','Opp','G','wins_so_far','losses_so_far','Passing_Att',
+            'Passing_Cmp', 'Passing_Yds', 'Passing_TD', 'Passing_Int',
+            'Rushing_Att', 'Rushing_Yds', 'Rushing_TD', 'Receiving_Rec',
+            'Receiving_Yds', 'Receiving_TD', 'Tackles_Solo', 'Tackles_Ast',
+            'Tackles_Tot', 'Tackles_Loss', 'Tackles_Sk', 'Def Int_Int',
+            'Def Int_Yds', 'Def Int_TD', 'Def Int_PD', 'Fumbles_FR', 
+            'Fumbles_Yds','Fumbles_TD', 'Fumbles_FF','height_inches','weight']
+means=samp.loc[:,x_numerical_cols].mean(axis=0)
+stds=samp.loc[:,x_numerical_cols].std(axis=0)
+
+
+
+x_categoricals=['team_rank','conference_href','home_away','w_l','Class','Pos']
+samp.loc[:,'team_rank'][samp.loc[:,'team_rank']=='']='NR'
+
+
+
+'height_inches', 'weight', 
+
+
+
+
+
+
+
 engine.dispose()
 del(engine)
 
