7d27d5993c17f6e5778ffc10f4cb3779e89477c5
Christopher Gonzalez <chrisgonza@gmail.com>
2022-07-21 14:56:52 -0500

    Update model_training.py
    just grab all the data instead

--- cfb_to_nfl_performance/model_training.py
+++ cfb_to_nfl_performance/model_training.py
@@ -15,7 +15,7 @@ import numpy as np
 os.chdir('/Users/chrisgonzalez/Documents/projects/cfb_to_nfl_performance/')
 
 engine = create_engine('postgresql://postgres:estarguars@localhost:5432/postgres')
-sql_query='select n.college_id,n.player_id,n."Player",n.first_year from nfl_player_snaps_table n where n.snapcounts > 49 and n.college_id is not null'
+sql_query='select n.* from nfl_player_snaps_table n where n.snapcounts > 49 and n.college_id is not null'
 
 samples=pd.read_sql_query(sql=sql_query, con=engine)
 #samples.groupby(['first_year']).count()
